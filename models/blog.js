4cdb298b2e3092f7c0a8880098c686e98205535b
DarthRL <darthrlu@gmail.com>
2023-05-28 18:39:48 +0800

    4.2

---
+++ models/blog.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const config = require('../utils/config')
+
+const url = config.MONGODB_URI
+
+mongoose.connect(url)
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+})
+
+
+module.exports = mongoose.model('Blog', blogSchema)

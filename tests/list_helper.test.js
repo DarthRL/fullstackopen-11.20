425051bd45c018bb9f67adee2278405a8489f512
DarthRL <darthrlu@gmail.com>
2023-05-28 21:01:59 +0800

    4.7

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -139,3 +139,27 @@ describe('author with most blogs', () => {
       })
   })
 })
+
+describe('author with most likes', () => {
+
+  test('of empty list is empty', () => {
+    const result = listHelper.mostLikes([])
+    expect(result).toEqual({})
+  })
+
+  test('when list has only one blog, equals the properties of that', () => {
+    const result = listHelper.mostLikes(listWithOneBlog)
+    expect(result).toEqual({
+      author: 'Edsger W. Dijkstra',
+      likes: 5
+    })
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.mostLikes(blogs)
+    expect(result).toEqual({
+      author: 'Edsger W. Dijkstra',
+      likes: 17
+    })
+  })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -38,6 +38,16 @@ const mostBlogs = (blogs) => {
     : {author:most[0], blogs:most[1]}
 }
 
+const mostLikes = (blogs) => {
+  const grouped = _.groupBy(blogs, 'author')
+  const summed = Object.entries(grouped).map((item) => [item[0], _.sumBy(item[1], 'likes')])
+  const most = _.sortBy(summed, 1).at(-1)
+  return blogs.length === 0
+    ? {}
+    : {author:most[0], likes:most[1]}
+
+}
+
 module.exports = {
-  dummy, totalLikes, favouriteBlog, mostBlogs
+  dummy, totalLikes, favouriteBlog, mostBlogs, mostLikes
 }

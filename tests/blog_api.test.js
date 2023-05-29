95c498f78eedd3ebd4711cafc75b1dd077b1b4d7
DarthRL <darthrlu@gmail.com>
2023-05-29 23:25:47 +0800

    4.10

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -29,6 +29,32 @@ test('identifier is named "id"', async () => {
   })
 }, 100000)
 
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: "new blog",
+    author: "new author",
+    url: "new url",
+    likes: 10
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  const blogs = response.body.map(r => ({
+    title: r.title,
+    author: r.author,
+    url: r.url,
+    likes: r.likes
+  }))
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+  expect(blogs).toContainEqual(newBlog)
+}, 100000)
 
 afterAll(async () => {
   await mongoose.connection.close()

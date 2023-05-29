401b70fbedd079c903494f49b566cc69fcc1267c
DarthRL <darthrlu@gmail.com>
2023-05-29 23:42:43 +0800

    4.12

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -56,6 +56,57 @@ test('a valid blog can be added', async () => {
   expect(blogs).toContainEqual(newBlog)
 }, 100000)
 
+test('missing "likes" defaults to 0', async () => {
+  const newBlog = {
+    title: "new blog",
+    author: "new author",
+    url: "new url"
+  }
+  const newBlogWithLikes = {
+    ...newBlog,
+    likes: 0
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  const blogs = response.body.map(r => ({
+    title: r.title,
+    author: r.author,
+    url: r.url,
+    likes: r.likes
+  }))
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+  expect(blogs).toContainEqual(newBlogWithLikes)
+}, 100000)
+
+test('missing title or url returns 400', async () => {
+  const newBlog1 = {
+    author: "new author",
+    url: "new url"
+  }
+  const newBlog2 = {
+    title: "new title",
+    author: "new author"
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog1)
+    .expect(400)
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog2)
+    .expect(400)
+}, 100000)
+
 afterAll(async () => {
   await mongoose.connection.close()
 })

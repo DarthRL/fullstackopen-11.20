ab5b5239324b51267e8aa0ba02568865b19cc246
DarthRL <darthrlu@gmail.com>
2023-05-30 22:23:31 +0800

    4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,15 +1,25 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  const body = request.body
+  const users = await User.find({})
+  const user = users[0]
+
+  const blog = new Blog({
+    ...body,
+    user: user.id
+  })
   if (blog.title && blog.url) {
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(blog._id)
+    await user.save()
     response.status(201).json(savedBlog)
   }
   else {
--- controllers/users.js
+++ controllers/users.js
@@ -6,14 +6,14 @@ usersRouter.post('/', async (request, response, next) => {
   const { username, name, password } = request.body
 
   if (password.length < 3) {
-    return response.status(400).json({ error: 'password must be at least 3 characters long'})
+    return response.status(400).json({ error: 'password must be at least 3 characters long' })
   }
 
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
 
   const user = new User({
-    username, 
+    username,
     name,
     passwordHash,
   })
@@ -27,7 +27,12 @@ usersRouter.post('/', async (request, response, next) => {
 })
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({}, {username:1, name:1, id:1})
+  const users = await User.find({}).populate("blogs", {
+    url: 1,
+    title: 1,
+    author: 1,
+    id: 1
+  })
   response.json(users)
 })
 

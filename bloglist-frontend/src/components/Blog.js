526c380bf8ee10b490da3a3a76cd920bd06989f6
DarthRL <darthrlu@gmail.com>
2023-06-04 19:41:53 +0800

    5.7

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,33 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [detailVisible, setDetailVisible] = useState(false)
+
+  const showWhenDetailVisible = { display: detailVisible ? '' : 'none' }
+  const buttonText = detailVisible ? 'hide' : 'view'
+
+  const toggleDetailVisible = () => {
+    setDetailVisible(!detailVisible)
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author} <button onClick={toggleDetailVisible}>{buttonText}</button>
+      <div style={showWhenDetailVisible}>
+        <div>{blog.url}</div>
+        <div>likes {blog.likes}<button>like</button></div>
+        <div>{blog.user.name}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
